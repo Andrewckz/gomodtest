c2b1b28fe9afa923fbe80e6e72f67a1e65ed6079
Andrewckz <756858127@qq.com>
2020-11-22 15:32:52 +0800

    add func

--- JiaMi.go
+++ JiaMi.go
@@ -6,8 +6,15 @@ import (
 )
 
 func JiaMi(pwd string) string {
+	pwdAndSalt := []byte(pwd)
+	apwd := fmt.Sprintf("%x", md5.Sum(pwdAndSalt))
+	fmt.Printf("加密后密码为: %v\n", apwd)
+	return apwd
+}
+
+func JiaMiBySalt(pwd string) string {
 	pwdAndSalt := []byte("salt1" + pwd)
 	apwd := fmt.Sprintf("%x", md5.Sum(pwdAndSalt))
-	fmt.Printf("加密后密码为: %x\n", apwd)
+	fmt.Printf("加密后密码为: %v\n", apwd)
 	return apwd
 }
